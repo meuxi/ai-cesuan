bba6cabadafaa53bb68df2d3e12b4c8c83119072
meuxi <meuxi@users.noreply.github.com>
2026-01-09 15:13:33 +0800

    更新前端配置和依赖，删除优化文档

--- src/user.py
+++ src/user.py
@@ -11,20 +11,22 @@ from src.models import User
 from fastapi import HTTPException
 
 _logger = logging.getLogger(__name__)
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 DEFAULT_TOKEN = ["xxx", "undefined"]
 
 
 def get_user(
-    credentials: HTTPAuthorizationCredentials = Depends(security)
+    credentials: Optional[HTTPAuthorizationCredentials] = Depends(security)
 ) -> Optional[User]:
     try:
+        if credentials is None:
+            return None
         jwt_token = credentials.credentials
         if not jwt_token or jwt_token in DEFAULT_TOKEN:
             return None
         payload = jwt.decode(
             jwt_token, settings.jwt_secret, algorithms=["HS256"])
-        jwt_payload = User.parse_obj(payload)
+        jwt_payload = User.model_validate(payload)
         if jwt_payload.expire_at < datetime.datetime.now().timestamp():
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Token 已过期，请重新登录")
